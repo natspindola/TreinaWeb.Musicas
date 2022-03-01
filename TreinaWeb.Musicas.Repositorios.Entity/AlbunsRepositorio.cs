27fe0915b834251093b059e8568662fa7d13772c
natspindola <natspindola@hotmail.com>
2022-03-01 20:18:52 -0300

    Repository Pattern pt3

---
+++ TreinaWeb.Musicas.Repositorios.Entity/AlbunsRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreinaWeb.Musicas.AcessoDados.Entity.Context;
+using TreinaWeb.Musicas.Dominio;
+using TreinaWeb.Repositorios.Comum.Entity;
+
+namespace TreinaWeb.Musicas.Repositorios.Entity
+{
+    public class AlbunsRepositorio : RepositorioGenericoEntity<Album, int>
+    {
+        public AlbunsRepositorio(MusicasDbContext contexto)
+            : base(contexto)
+        {
+
+        }
+    }
+}
