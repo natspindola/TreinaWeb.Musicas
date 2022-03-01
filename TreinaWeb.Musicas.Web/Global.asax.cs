8439c2f937f2026fa59dd807ef3f2b14263c8be4
natspindola <natspindola@hotmail.com>
2022-03-01 18:38:08 -0300

    AutoMapper pt1

--- TreinaWeb.Musicas.Web/Global.asax.cs
+++ TreinaWeb.Musicas.Web/Global.asax.cs
@@ -5,17 +5,21 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TreinaWeb.Musicas.Web.App_Start;
 
 namespace TreinaWeb.Musicas.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public object AutoMapperConf { get; private set; }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutoMapperConfig.Configurar();
         }
     }
 }
