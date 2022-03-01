a6db9c607f6ff9a5d2f8b2a9622021b5ec90e56b
natspindola <natspindola@hotmail.com>
2022-03-01 18:20:12 -0300

    pattern ViewModel

---
+++ TreinaWeb.Musicas.Web/ViewModels/Album/AlbumIndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TreinaWeb.Musicas.Web.ViewModels.Album
+{
+    public class AlbumIndexViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nome do albúm")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Ano do albúm")]
+        public int Ano { get; set; }
+
+        [Display(Name = "Observação do albúm")]
+        public string Observações { get; set; }
+
+        [Display(Name = "Email de contato")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
